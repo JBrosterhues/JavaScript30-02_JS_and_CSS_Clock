1b5c9811ac55ff3ff6000283ce83ce9fa007936b
JBrosterhues <juebro2004@live.de>
2019-04-17 15:01:55 +0200

    Added weekday and date information.

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 function moveHands() {
+    let weekdayArr = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
+    let monthArr = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
     let hourHand = document.getElementById('hour-hand');
     let minuteHand = document.getElementById('minute-hand');
     let secondHand = document.getElementById('second-hand');
     let currDate = new Date();
+    let currYear = currDate.getFullYear();
+    let currMonth = currDate.getMonth();
+    let currDay = currDate.getDate();
+    let currWeekday = currDate.getDay();
+    let dateText =
+        weekdayArr[currWeekday] +
+        ', ' +
+        currDay +
+        '. ' +
+        monthArr[currMonth] +
+        ' ' +
+        currYear;
+    let dateElement = document.getElementById('date-text');
+    dateElement.innerHTML = dateText;
     let currHour = currDate.getHours();
     let currMinute = currDate.getMinutes();
     let currSecond = currDate.getSeconds();
